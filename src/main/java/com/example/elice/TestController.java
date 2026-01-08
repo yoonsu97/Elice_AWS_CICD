4994438f1e210381b5240d8277f30834172418b0
[CLOUD6] 박윤수 <aspv12@nate.com>
2026-01-08 16:29:12 +0900

    initTest

--- src/main/java/com/example/elice/TestController.java
+++ src/main/java/com/example/elice/TestController.java
@@ -15,7 +15,7 @@ public class TestController {
     @CrossOrigin(origins = "*", methods = {RequestMethod.POST, RequestMethod.GET, RequestMethod.OPTIONS})
     @GetMapping
     public String home() {
-    return "Hello Elice!!!!!!!!!!!!!!!!!!!!!!!!!!";
+    return "Hello Elice!!!!!!!!!!!!!!!!!!!!!!!!!! test";
     }
 
     @CrossOrigin(origins = "*", methods = {RequestMethod.POST, RequestMethod.GET, RequestMethod.OPTIONS})
